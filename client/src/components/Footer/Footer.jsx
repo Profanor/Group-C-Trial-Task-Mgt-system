f0219202ffdefb83256e066fe78877664a3297e7
David Arinze <fivelanes72@gmail.com>
2024-03-29 08:36:20 +0100

    designed the footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import './Footer.css';
+import { GitHub, LinkedIn, Twitter, Email } from '@material-ui/icons';
 
 const Footer = () => {
   return (
-    <div className='footer-container'>
+    <footer className='footer-container'>
         <div className='flex'>
-            <div className='one'></div>
-            <div className='two'></div>
-            <div className='three'></div>
-            <div className='four'></div>
+            <div className='controls'>
+            <div className='one'>
+                <div className='rounded'>
+                    <GitHub style={{ color: 'black', fontSize: 40 }} />
+                </div>
+                <div><h3 className='label'>GitHub</h3></div>
+            </div>
+            <div className='two'>
+                <div className='rounded'>
+                    <LinkedIn style={{ color: 'black', fontSize: 40 }} />
+                </div>
+                <div><h3 className='label'>LinkedIn</h3></div>
+            </div>
+            <div className='three'>
+                <div className='rounded'>
+                    <Twitter style={{ color: 'black', fontSize: 40 }} />
+                </div>
+                <div><h3 className='label'>Twitter</h3></div>
+            </div>
+            <div className='four'>
+                <div className='rounded'>
+                    <Email style={{ color: 'black', fontSize: 40 }} />
+                </div>
+                <div><h3 className='label'>Email</h3></div>
+            </div>
         </div>
     </div>
+</footer>
   )
 }
 
