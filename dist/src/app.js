d0acc890b1b9af30f19b9af740f640efad0bd558
David Arinze <fivelanes72@gmail.com>
2024-03-30 16:14:24 +0100

    modified the app.ts file to serve directly from the client folder

--- dist/src/app.js
+++ dist/src/app.js
@@ -20,29 +20,34 @@ app.use((0, cors_1.default)({
     origin: FRONTEND_URL,
     credentials: true
 }));
-//other middleware
+// Middleware setup
 app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, "..", 'public')));
-//Routes
+// Serve static files from the React app
+app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'client', 'build')));
+// Routes
 const index_1 = __importDefault(require("./routes/index"));
 const users_1 = __importDefault(require("./routes/users"));
-//use your routes
+// Use your routes
 app.use('/', index_1.default);
 app.use('/users', users_1.default);
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next((0, http_errors_1.default)(404));
 });
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    // Log the error
+    console.error(err);
+    // Send an error response
+    res.status(err.status || 500).json({
+        message: err.message || 'Internal Server Error'
+    });
+});
+// Serve the React app for any other route
+app.get('*', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -4,52 +4,59 @@ import createError from 'http-errors';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import main from '../config/db'
-import cors from 'cors'
+import main from '../config/db';
+import cors from 'cors';
 
 const app = express();
 
 main().catch((err) => {
   console.error(err);
-  process.exit(1); 
+  process.exit(1);
 });
 
-const FRONTEND_URL = process.env.FRONTEND_URL
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(cors({
   origin: FRONTEND_URL,
-   credentials: true
+  credentials: true
 }));
 
-//other middleware
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,"..",'public')));
 
-//Routes
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+// Routes
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
-//use your routes
+// Use your routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function( err: any, req: Request, res: Response, next: NextFunction ) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Error handler
+app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
+  // Log the error
+  console.error(err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // Send an error response
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error'
+  });
+});
+
+// Serve the React app for any other route
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 export default app;
